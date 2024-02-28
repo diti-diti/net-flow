2b7551d003f8f7744b3c33ab0415c5736e7cca8a
marcin.dittmann <marcin.dittmann@gmail.com>
2024-02-28 11:20:04 +0100

    env modification

--- telegraf/run.sh
+++ telegraf/run.sh
@@ -4,7 +4,7 @@ echo "=> Starting iperf3 test..."
 #iperf3 -c 192.168.0.68 -i1 -t600 --json
 #iperf3 -c 192.168.0.68 -i1 -t36000 -R --json
 
-# Sprawdź status wyjścia iperf3; jeśli nie jest równy 0, wystąpił błąd.
+# Check the exit status of iperf3; if it's not equal to 0, an error occurred.
 # if [ $? -ne 0 ]; then
 #     echo "iperf3 failed, exiting..."
 #     exit 1
@@ -12,4 +12,4 @@ echo "=> Starting iperf3 test..."
 
 echo "=> Starting Telegraf ..."
 telegraf --config /etc/telegraf/telegraf.conf 
-# Opcjonalnie: --config-directory /etc/telegraf/telegraf.d
+# Optional: --config-directory /etc/telegraf/telegraf.d
